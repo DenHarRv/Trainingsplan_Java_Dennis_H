b70fe21e84d1c37d1638906e0f457f74ba083936
dennis <dsharrer97@gmail.com>
2026-04-14 20:45:12 +0200

    Initial commit

--- src/Woche3.java
+++ src/Woche3.java
@@ -4,5 +4,14 @@ public class Woche3 {
     public static void main(String[] args) {
 
 
+
+        begruesseGast("John Wayne");
+    }
+
+
+
+    public static void begruesseGast(String name) {
+
+        System.out.println("Willkommen im Sallon, " + name + "!");
     }
 }
